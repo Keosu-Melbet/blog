06f9d33666c56f3df865efc0d108fca50bc22b37
Keosu-Melbet <melbet.globalaffiliates@gmail.com>
2025-09-11 17:33:14 +0300

    Update forms.py

--- app/forms.py
+++ app/forms.py
@@ -1,35 +1,17 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, TextAreaField, SelectField, BooleanField, SubmitField
-from wtforms.validators import DataRequired, Length, Email
+from wtforms import StringField, TextAreaField, BooleanField, SelectField
+from wtforms.validators import DataRequired
 
 class ArticleForm(FlaskForm):
-    title = StringField('Tiêu đề', validators=[DataRequired(), Length(max=200)])
-    content = TextAreaField('Nội dung', validators=[DataRequired()])
-    excerpt = TextAreaField('Tóm tắt', validators=[Length(max=300)])
-    category_id = SelectField('Chuyên mục', coerce=int, validators=[DataRequired()])
-    featured_image = StringField('Ảnh đại diện (URL)')
-    featured = BooleanField('Bài viết nổi bật')
-    published = BooleanField('Xuất bản', default=True)
-    
-    # SEO fields
-    meta_title = StringField('Meta Title', validators=[Length(max=200)])
-    meta_description = TextAreaField('Meta Description', validators=[Length(max=300)])
-    meta_keywords = StringField('Meta Keywords', validators=[Length(max=500)])
-    
-    submit = SubmitField('Lưu bài viết')
-    
-    def __init__(self, *args, **kwargs):
-        super(ArticleForm, self).__init__(*args, **kwargs)
-        # Populate category choices
-        self.category_id.choices = [(cat.id, cat.name) for cat in Category.query.all()]
+    title = StringField("Tiêu đề", validators=[DataRequired()])
+    content = TextAreaField("Nội dung", validators=[DataRequired()])
+    category_id = SelectField("Danh mục", choices=[], coerce=int)
+    published = BooleanField("Công khai")
 
 class ContactForm(FlaskForm):
-    name = StringField('Họ tên', validators=[DataRequired(), Length(max=100)])
-    email = StringField('Email', validators=[DataRequired(), Email(), Length(max=120)])
-    subject = StringField('Chủ đề', validators=[DataRequired(), Length(max=200)])
-    message = TextAreaField('Tin nhắn', validators=[DataRequired(), Length(max=1000)])
-    submit = SubmitField('Gửi tin nhắn')
+    name = StringField("Tên", validators=[DataRequired()])
+    email = StringField("Email", validators=[DataRequired()])
+    message = TextAreaField("Tin nhắn", validators=[DataRequired()])
 
 class SearchForm(FlaskForm):
-    query = StringField('Tìm kiếm...', validators=[DataRequired(), Length(max=100)])
-    submit = SubmitField('Tìm kiếm')
+    keyword = StringField("Từ khóa", validators=[DataRequired()])
