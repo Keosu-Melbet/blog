8fe28c2b9efc9400d2375fc47244a1e3230fffa9
Keosu-Melbet <melbet.globalaffiliates@gmail.com>
2025-09-11 16:34:27 +0300

    Update models.py

--- app/models.py
+++ app/models.py
@@ -1,7 +1,5 @@
-
 from flask import render_template, request, redirect, url_for, flash, jsonify, make_response
 from . import db
-from .models import Article, Category, BettingOdd, Match
 from forms import ArticleForm, ContactForm, SearchForm
 from seo_utils import generate_meta_tags
 from datetime import datetime, timedelta
