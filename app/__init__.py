a950e9bb63b919425d1ba890ff38ae853397c4cc
Keosu-Melbet <melbet.globalaffiliates@gmail.com>
2025-09-11 14:16:19 +0300

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,18 +1,25 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.middleware.proxy_fix import ProxyFix
 
 db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
-    app.config.from_object("config.Config")
+    app.secret_key = os.environ.get("SESSION_SECRET", "dev-key")
+    app.wsgi_app = ProxyFix(app.wsgi_app, x_proto=1, x_host=1)
 
+    # Config
+    app.config.from_object("config")
+
+    # DB
     db.init_app(app)
 
-    # Đăng ký routes
+    # Blueprints
     from .routes import main_bp
     from .admin_routes import admin_bp
     app.register_blueprint(main_bp)
-    app.register_blueprint(admin_bp)
+    app.register_blueprint(admin_bp, url_prefix="/admin")
 
     return app
