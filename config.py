b90da8a25cda25d64f53ef2e16097ce0f6e46b01
Keosu-Melbet <melbet.globalaffiliates@gmail.com>
2025-09-11 17:55:05 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,16 +1,7 @@
-import os
-
-class Config:
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL", "sqlite:///keosu.db")
+class DevelopmentConfig:
+    SQLALCHEMY_DATABASE_URI = "sqlite:///instance/app.db"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_ENGINE_OPTIONS = {
-        "pool_recycle": 300,
-        "pool_pre_ping": True,
-    }
-    SECRET_KEY = os.environ.get("SESSION_SECRET", "dev-key")
 
-class ProductionConfig(Config):
-    DEBUG = False
-
-class DevelopmentConfig(Config):
-    DEBUG = True
+class ProductionConfig:
+    SQLALCHEMY_DATABASE_URI = "sqlite:///instance/app.db"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
