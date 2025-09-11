574eaf4f7a567ef2b42549a11c2e7459ebcf5c62
Keosu-Melbet <melbet.globalaffiliates@gmail.com>
2025-09-11 17:56:05 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,18 @@
-class DevelopmentConfig:
-    SQLALCHEMY_DATABASE_URI = "sqlite:///instance/app.db"
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config:
+    SECRET_KEY = os.environ.get("SESSION_SECRET", "dev-key")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-class ProductionConfig:
-    SQLALCHEMY_DATABASE_URI = "sqlite:///instance/app.db"
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+class DevelopmentConfig(Config):
+    DEBUG = True
+    FLASK_ENV = "development"
+    SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(basedir, "instance", "app.db")
+
+class ProductionConfig(Config):
+    DEBUG = False
+    FLASK_ENV = "production"
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL", "sqlite:///" + os.path.join(basedir, "instance", "app.db"))
+
