463c55cba83769e3ff004bfcaee2729ec4e62c06
Keosu-Melbet <melbet.globalaffiliates@gmail.com>
2025-09-11 18:12:59 +0300

    Update init_data.py

--- init_data.py
+++ init_data.py
@@ -1,7 +1,9 @@
 from app import create_app, db
 
+# Khởi tạo Flask app từ factory
 app = create_app()
 
+# Tạo bảng trong database nếu chưa có
 with app.app_context():
     db.create_all()
-    print("✅ Database initialized")
+    print("✅ Database initialized successfully")
