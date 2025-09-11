05a8b7012883eb5476cd7549c9fde15490bf3074
Keosu-Melbet <melbet.globalaffiliates@gmail.com>
2025-09-11 18:04:49 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,4 @@
 import os
-
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
@@ -8,11 +7,8 @@ class Config:
 
 class DevelopmentConfig(Config):
     DEBUG = True
-    FLASK_ENV = "development"
     SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(basedir, "instance", "app.db")
 
 class ProductionConfig(Config):
     DEBUG = False
-    FLASK_ENV = "production"
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL", "sqlite:///" + os.path.join(basedir, "instance", "app.db"))
-
