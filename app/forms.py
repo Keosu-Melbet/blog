04809746a96cb9066b1c33b07ac9f0e85cf47bf3
Keosu-Melbet <melbet.globalaffiliates@gmail.com>
2025-09-12 13:10:43 +0300

    Update forms.py

--- app/forms.py
+++ app/forms.py
@@ -1,12 +1,12 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, TextAreaField, BooleanField, SelectField
+from wtforms import StringField, TextAreaField, BooleanField, SelectField, SubmitField
 from wtforms.validators import DataRequired, Email
 
 class ArticleForm(FlaskForm):
     title = StringField("Tiêu đề", validators=[DataRequired()])
     content = TextAreaField("Nội dung", validators=[DataRequired()])
-    category_id = SelectField("Danh mục", choices=[], coerce=int)
-    published = BooleanField("Công khai")
+    category = SelectField("Nhóm danh mục", choices=[], validators=[DataRequired()])
+    published = BooleanField("Có hiển thị")
 
 class ContactForm(FlaskForm):
     name = StringField("Họ và tên", validators=[DataRequired()])
